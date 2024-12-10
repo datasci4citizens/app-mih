1038fcec252c20dcbef5320127da04cb7c5fb9cf
Gabriel <g281770@dac.unicamp.br>
2024-12-10 11:10:52 -0300

    specialist guard: verify 'is_allowed'

--- src/guards/specialist.tsx
+++ src/guards/specialist.tsx
@@ -1,5 +1,6 @@
+import { Button } from "@/components/ui/button";
 import { useUser } from "@/lib/hooks/use-user";
-import { Navigate, Outlet } from "react-router-dom";
+import { Link, Navigate, Outlet } from "react-router-dom";
 
 export function SpecialistGuard() {
 
@@ -7,6 +8,24 @@ export function SpecialistGuard() {
 
     if (data.role != "specialist")
         return <Navigate to='/' />
+    else if (!data.is_allowed) {
+        return (
+            <div className="h-screen min-w-full flex flex-col justify-start items-center p-[30px] pt-[100px] bg-primary">
+
+                <div className="flex flex-col justify-between items-center h-[60%]">
+                    <h1 className="text-4xl font-bold text-white text-start">Ops! <br /> Parece que você ainda não tem permissão para acessar a área de especialista </h1>
+
+                    <Link to="/specialist/home">
+                        <Button variant={"secondary"} className="text-2xl text-primary">
+                            Retornar
+                        </Button>
+                    </Link>
+
+                </div>
+            </div>
+        )
+
+    }
 
     return <Outlet />
 }
