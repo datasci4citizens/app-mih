b6f0f62118b6a2a1f65d55fb541865820bcf4961
Marcos Menezes <marcosnunes12340987@gmail.com>
2025-12-04 16:55:33 -0300

    remove proxy

--- vite.config.ts
+++ vite.config.ts
@@ -10,13 +10,4 @@ export default defineConfig({
 			'@': path.resolve(__dirname, './src'),
 		},
 	},
-	// server: {
-	// 	proxy: {
-	// 		'/api': {
-	// 			target: 'https://unretiring-terri-unfunded.ngrok-free.dev',
-	// 			changeOrigin: true,
-	// 			rewrite: (path) => path.replace(/^\/api/, ''),
-	// 		},
-	// 	},
-	// },
 });
