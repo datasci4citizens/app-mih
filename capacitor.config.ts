ea183b0c3610d8f2d9a9010141475d7db00755ca
Marcos Menezes <marcosnunes12340987@gmail.com>
2026-08-14 12:24:35 -0300

    update app version

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,17 @@
 import type { CapacitorConfig } from '@capacitor/cli';
+import 'dotenv/config';
 
 const config: CapacitorConfig = {
 	appId: 'br.unicamp.ic.mih',
 	appName: 'mih',
 	webDir: 'dist',
+	plugins: {
+		SocialLogin: {
+			google: {
+				webClientId: process.env.VITE_GOOGLE_CLIENT_ID
+			},
+		},
+	},
 };
 
 export default config;
